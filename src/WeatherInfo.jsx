9968c69ea683e19e4a63fcbe5bbf87325a9c0d48
rahulswain85 <rahul8033swain@gmail.com>
2025-12-23 18:05:44 +0530

    Weather images

--- src/WeatherInfo.jsx
+++ src/WeatherInfo.jsx
@@ -8,10 +8,9 @@ import Typography from "@mui/material/Typography";
 import "./WeatherInfo.css"
 
 export default function Weatherinfo({weatherInfo: info}) {
-  
-
-  const INIT_URL =
-    "https://images.unsplash.com/photo-1722858343990-1604f540c15d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    let hot_image = "./src/assets/hot.png";
+    let cold_image = "./src/assets/cold.png";
+    let rainy_image = "./src/assets/rainy.png";
   return (
     <>
       {info.feelslike != null ? (
@@ -21,7 +20,7 @@ export default function Weatherinfo({weatherInfo: info}) {
             {info.weatherDesc.toUpperCase()}
           </h2>
           <Card sx={{ maxWidth: 345 }}>
-            <CardMedia sx={{ height: 140 }} image="./src/assets/weather.png" />
+            <CardMedia sx={{ height: 140 }} image={info.humidity > 80 ? rainy_image : info.temp > 13 ? hot_image : cold_image} />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
                 {info.city}
